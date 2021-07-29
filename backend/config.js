0c3bd0f92947fff40f221174c10ec3db549716a9
palkinjuneja <palkinjuneja1234@gmail.com>
2021-07-29 21:00:38 +0530

    added prod env

--- backend/config.js
+++ backend/config.js
@@ -5,6 +5,8 @@
       'clientID': '86q8czzy4de81z', // your App ID
       'clientSecret': 'MAkDC6DTdALy0cuq', // your App Secret
       'callbackURL':'http://127.0.0.1:9000/auth/linkedin/callback'
+      // uncomment the below line for prod and comment the above line
+      //'callbackURL':process.env.LINKEDIN_REDIRECT+'/auth/linkedin/callback'
       
     }
     
